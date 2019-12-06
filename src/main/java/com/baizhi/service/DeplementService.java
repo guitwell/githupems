19c2aefdd2be2287c0dd1f14c998c0487fd5210d
huangchen <1538248010@qq.com>
2019-12-06 16:35:58 +0800

    service写完

--- src/main/java/com/baizhi/service/DeplementService.java
+++ src/main/java/com/baizhi/service/DeplementService.java
@@ -1,9 +1,20 @@
 package com.baizhi.service;
 
+import com.baizhi.entity.Deplement;
+
+import java.util.List;
+
 /**
  * author : 张京斗
  * create_date : 2019/12/6 11:29
  * version : 1.0
  */
 public interface DeplementService {
+    List<Deplement> findByPage(Integer nowpage, Integer pagesize);
+
+    void addOne(Deplement deplement);
+
+    void updateOne(Deplement deplement);
+
+    void deleteOne(String deptid);
 }
---
+++ src/main/java/com/baizhi/service/DeplementServiceImpl.java
@@ -0,0 +1,46 @@
+package com.baizhi.service;
+
+import com.baizhi.dao.DeplementDao;
+import com.baizhi.entity.Deplement;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * author : 张京斗
+ * create_date : 2019/12/6 16:32
+ * version : 1.0
+ */
+@Service
+@Transactional
+public class DeplementServiceImpl implements DeplementService {
+    @Autowired
+    private DeplementDao deplementDao;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<Deplement> findByPage(Integer nowpage, Integer pagesize) {
+        Deplement deplement = new Deplement();
+        Integer thebegin = (nowpage - 1) * pagesize;
+        return deplementDao.selectByRowBounds(deplement, new RowBounds(thebegin, pagesize));
+    }
+
+    @Override
+    public void addOne(Deplement deplement) {
+        deplementDao.insert(deplement);
+    }
+
+    @Override
+    public void updateOne(Deplement deplement) {
+        deplementDao.updateByPrimaryKeySelective(deplement);
+    }
+
+    @Override
+    public void deleteOne(String deptid) {
+        deplementDao.deleteByPrimaryKey(deptid);
+    }
+}
